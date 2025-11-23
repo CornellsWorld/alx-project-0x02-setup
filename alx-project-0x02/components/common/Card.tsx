09dfe4923e6cb5fe1af0dedda7e38e56a529a222
CornellsWorld <corneliusalhassan@gmail.com>
2025-11-23 21:19:08 +0100

    update

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,10 +1,10 @@
-import { CardProps } from "../../interfaces";
+import { type CardProps } from "@/interfaces";
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
     <div className="border p-4 rounded-lg shadow-md bg-white">
-      <h2 className="text-2xl font-bold mb-2">{title}</h2>
-      <p className="text-gray-700">{content}</p>
+      <h2 className="text-xl font-bold">{title}</h2>
+      <p>{content}</p>
     </div>
   );
 };
