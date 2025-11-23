063b2370c18760e2525c6894fc3e574561210cfc
CornellsWorld <corneliusalhassan@gmail.com>
2025-11-23 20:18:12 +0100

    update

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,9 +1,11 @@
-
+import Header from "../components/layout/Header";
+import Link from "next/link";
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col h-screen">
-      
+      <Header />
+
       <main className="flex-grow flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
         <div className="text-center">
           <h1 className="text-5xl font-bold text-white">
@@ -12,13 +14,16 @@ const Home: React.FC = () => {
           <p className="mt-4 text-xl text-white">
             We're glad you're here. Explore and enjoy your experience.
           </p>
-          <button className="mt-6 px-6 py-3 bg-white text-blue-500 rounded-full font-semibold hover:bg-gray-200 transition">
-            Get Started
-          </button>
-            </div>
+          <Link href="/home">
+              <button className="mt-6 px-6 py-3 bg-white text-blue-500 rounded-full font-semibold   hover:bg-gray-200 transition">
+                Get Started
+              </button>
+          </Link>
+          
+        </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
